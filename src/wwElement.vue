5053f4f8e71e0d165c33c6fb6f0b51d4f27b5480
Alexis JC <alexisjc.dev@gmail.com>
2024-07-10 12:28:07 +0200

    fix: use create no longer exist

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -68,7 +68,11 @@ export default {
 
         const input = ref(null);
 
-        return { variableValue, setValue, type, input };
+        /* wwEditor:start */
+        const { createElement } = wwLib.wwElement.useCreate();
+        /* wwEditor:end */
+
+        return { variableValue, setValue, type, input, createElement };
     },
     data() {
         return {
@@ -229,7 +233,7 @@ export default {
                 let placeholderElement = null;
 
                 if (value) {
-                    placeholderElement = await wwLib.createElement(
+                    placeholderElement = await createElement(
                         'ww-text',
                         {},
                         { name: 'Placeholder' },
