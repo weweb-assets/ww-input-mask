052cfa8523494a6855c406ac0426c6d73659ca43
Léo Coletta <mail.leo.coletta@gmail.com>
2025-07-09 09:23:31 +0200

    Merge pull request #20 from weweb-assets/fix/WW-3871-disable-custom-validation-bindable
    [WW-3871] Disable bindable for customValidation property

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -67,6 +67,7 @@ export default {
     },
     emits: ['trigger-event', 'add-state', 'remove-state', 'update:content:effect'],
     setup(props, { emit }) {
+
         const type = computed(() => {
             if (Object.keys(props.wwElementState.props).includes('type')) {
                 return props.wwElementState.props.type;
